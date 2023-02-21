11b7913be254ca397079bc556861066197ca5057
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-02-21 21:50:14 +0100

    added card component, added card.css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,8 +4,8 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
+        <ExpenseDate date={props.date} />
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
