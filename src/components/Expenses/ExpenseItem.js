9b71cd009922fdddcbedeeaa1e27e97095e3c699
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-02 00:34:56 +0100

    addes use state to ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,16 +5,18 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  // const clickHandler = () => {
-  //   console.log("clicked!!!");
-  // };
+  const [title, setTitle] = useState(props.title);
 
-  useState();
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={props.date} />
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
