66c68c06715c65b9c4e6483c0ebadab5a807599d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-03-01 23:59:56 +0100

    changes to expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  // const clickHandler = () => {
+  //   console.log("clicked!!!");
+  // };
+
+  useState();
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
@@ -10,6 +17,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
